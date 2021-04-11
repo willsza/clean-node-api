8b035a6dc84dba1bb37ef1c1030441b9714fbbed
wilfrank84 <wilfrank84@gmail.com>
2021-04-11 19:12:15 -0300

    chore: add jest configs and scripts

---
+++ jest-integration-config.js
@@ -0,0 +1,4 @@
+/* eslint-disable semi */
+var config = require('./jest.config');
+config.testMatch = ['**/*.test.ts'];
+module.exports = config;
---
+++ jest-unit-config.js
@@ -0,0 +1,4 @@
+/* eslint-disable semi */
+var config = require('./jest.config');
+config.testMatch = ['**/*.spec.ts'];
+module.exports = config;
