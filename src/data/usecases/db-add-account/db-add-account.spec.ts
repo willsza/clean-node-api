655f3cf965ff80bc5316efe633024e135cd28b15
wilfrank84 <wilfrank84@gmail.com>
2021-04-12 20:58:11 -0300

    refactor: move sut to a helper method

--- src/data/usecases/db-add-account/db-add-account.spec.ts
+++ src/data/usecases/db-add-account/db-add-account.spec.ts
@@ -1,16 +1,30 @@
 import { AddAccountModel } from '../../../domain/usecases/add-account'
 import { DbAddAccount } from './db-add-account'
 
+interface SubTypes {
+  sut: DbAddAccount
+  encrypterStub: EncrypterStub
+}
+
+class EncrypterStub {
+  async encrypt (value: string): Promise<string> {
+    return new Promise(resolve => resolve('hashed_password'))
+  }
+}
+
+const makeSut = (): SubTypes => {
+  const encrypterStub = new EncrypterStub()
+  const sut = new DbAddAccount(encrypterStub)
+
+  return {
+    sut,
+    encrypterStub
+  }
+}
+
 describe('DBAddAccount Usecase', () => {
   test('should call Encrypter with correct value', async () => {
-    class EncrypterStub {
-      async encrypt (value: string): Promise<string> {
-        return new Promise(resolve => resolve('hashed_password'))
-      }
-    }
-
-    const encrypterStub = new EncrypterStub()
-    const sut = new DbAddAccount(encrypterStub)
+    const { sut, encrypterStub } = makeSut()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     const account: AddAccountModel = {
       name: 'valid_name',
