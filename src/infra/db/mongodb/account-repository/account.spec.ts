343a7db603d7ec0a60610575af7b1b89cdefc882
wilfrank84 <wilfrank84@gmail.com>
2021-04-16 19:22:11 -0300

    test: ensure AccountMongoRepository return an account on sucesses

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,31 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository()
+}
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('should AccountMongoRepository return an account on success', async () => {
+    const sut = makeSut()
+    const account = await sut.add({
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'hashed_password'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('valid_name')
+    expect(account.email).toBe('valid_email@email.com')
+    expect(account.password).toBe('hashed_password')
+  })
+})
